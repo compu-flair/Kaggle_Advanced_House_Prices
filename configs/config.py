f9706449edddb13b8773dfc2b915b2fb4d08849a
Ahmed Karem Mousa <akaremmousa@gmail.com>
2025-08-25 17:42:54 +0300

    feat(streamlit): XGBoost

--- configs/config.py
+++ configs/config.py
@@ -3,3 +3,6 @@ CUSTOM_APP_TAB = "Custom Linear Regression"
 
 # Tab name for the house price prediction application
 HOUSE_PRICE_TAB = "House Price Prediction"
+
+
+XGBOOST_APP_TAB = "Custom XGBoost"
--- main.py
+++ main.py
@@ -1,31 +1,34 @@
 import streamlit as st
 from views.house_price import run_house_price_app
-from views.custom_app import custom_model_linear_regression_app
-from configs.config import HOUSE_PRICE_TAB, CUSTOM_APP_TAB
+from views.custom_linear_app import custom_model_linear_regression_app
+from views.custom_xgboost import custom_model_xgboost_app
+from configs.config import HOUSE_PRICE_TAB, CUSTOM_APP_TAB, XGBOOST_APP_TAB
 
-# Set Streamlit page configuration
+# Configure Streamlit page settings
 st.set_page_config(page_title="House Prices App", layout="wide")
 
-# Display the main title
-st.title("Linear Regression Module")
+# Main page title
+st.title("Linear Regression & XGBoost Module")
 
-# Define tab names for navigation
-tab_names = [HOUSE_PRICE_TAB, CUSTOM_APP_TAB, "About"]
-# house_price_tab,custom_tab,about_tab = st.tabs(tab_names)  # (Unused tabs implementation)
+# Tab names for sidebar navigation
+tab_names = [HOUSE_PRICE_TAB, CUSTOM_APP_TAB, XGBOOST_APP_TAB, "About"]
 
-# Sidebar navigation
+# Sidebar for navigation between tabs
 with st.sidebar:
     st.header("Navigation")
     selected_tab = st.radio("Select a tab:", tab_names, index=0)
 
-# Render content based on selected tab
+# Display content based on selected tab
 if selected_tab == tab_names[0]:
     st.session_state["selected_tab"] = HOUSE_PRICE_TAB
-    run_house_price_app()  # Run house price prediction app
+    run_house_price_app()  # House price prediction module
 elif selected_tab == tab_names[1]:
     st.session_state["selected_tab"] = CUSTOM_APP_TAB
-    custom_model_linear_regression_app()  # Run custom linear regression app
+    custom_model_linear_regression_app()  # Custom linear regression module
 elif selected_tab == tab_names[2]:
+    st.session_state["selected_tab"] = XGBOOST_APP_TAB
+    custom_model_xgboost_app()  # Custom XGBoost module
+elif selected_tab == tab_names[3]:
     st.session_state["selected_tab"] = "About"
     st.header("About")
     st.write("This app demonstrates house price prediction using Streamlit.")  # About section
